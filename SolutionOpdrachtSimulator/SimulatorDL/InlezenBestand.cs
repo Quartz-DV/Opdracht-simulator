a03923ff519431f2794748af2a6ec6651836d954
Quartz-DV <238630229+Quartz-DV@users.noreply.github.com>
2026-03-13 12:26:23 +0100

    Verdere aanpassingen in de Bestandslezer

--- SolutionOpdrachtSimulator/SimulatorDL/InlezenBestand.cs
+++ SolutionOpdrachtSimulator/SimulatorDL/InlezenBestand.cs
@@ -16,31 +16,90 @@ namespace SimulatorDL
 
         }
 
-        
-        public LeesBestanden(string para) {
+
+        public List<string> ben (string para) {
 
             using (StreamReader adresInfo = new StreamReader(Path.Combine(para, "adresinfo.txt"))) {
 
                 string line;
                 adresInfo.ReadLine();
 
-                while ((line=adresInfo.ReadLine()) != null) {
+                while ((line = adresInfo.ReadLine()) != null) {
 
-                    string ls = line.Split(',');
-                    int 
+                    string[] ls = line.Trim().Split(',');
+                    string provincie = ls[0];
+                    string gemeenteNaam = ls[1];
+                    string straatNaam = ls[2];
 
 
                 }
 
             }
 
+            return null;
 
+            //using (StreamReader zipcodes = new StreamReader(Path.Combine(para, "zipcodes_alpha_nl_2025.csv"))) {
+
+            //    string line;
+            //    zipcodes.ReadLine();
+
+            //    while ((line = zipcodes.ReadLine()) != null) {
+
+            //        string[] ls = line.Trim().Split(';');
+            //        int postcode = int.Parse(ls[0]);
+            //        string gemeenteNaam = ls[3];
+
+            //    }
+            //}
+
+            //using (StreamReader familieNamen = new StreamReader(Path.Combine(para, "Familienamen_2024_Belgie.csv"))) {
+
+            //    string line;
+            //    familieNamen.ReadLine();
+
+            //    while ((line = familieNamen.ReadLine()) != null) {
+
+            //        string[] ls = line.Trim().Split(";");
+            //        int id = int.Parse(ls[0]);
+            //        string familieNaam = ls[1];
+
+            //    }
+
+            //}
+
+            //using (StreamReader mannenNamen = new StreamReader(Path.Combine(para, "mannennamen_belgie.csv"))) {
+
+            //    string line;
+            //    mannenNamen.ReadLine();
+
+            //    while ((line = mannenNamen.ReadLine()) != null) {
+
+            //        string[] ls = line.Trim().Split(";");
+            //        int id = int.Parse(ls[0]);
+            //        string mannenVoornaam = ls[1];
+
+            //    }
+
+            //}
+
+            //using (StreamReader vrouwenNamen = new StreamReader(Path.Combine(para, "vrouwennamen_belgie.csv"))) {
+
+            //    string line;
+            //    vrouwenNamen.ReadLine();
+
+            //    while ((line = vrouwenNamen.ReadLine()) != null) {
+
+            //        string[] ls = line.Trim().Split(";");
+            //        int id = int.Parse(ls[0]);
+            //        string vrouwenVoornaam = ls[1];
+
+
+
+
+                }
+
+
+            }
         }
-   
 
 
-
-
-
-    }
-}
